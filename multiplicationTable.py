98d10c2f0022ff15d93e806d8211a135eea45017
wudi <wudi@oriental-finance.com>
2017-08-03 09:27:23 +0800

    添加右上和右下99乘法表

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -6,7 +6,7 @@ def leftBelowMultiplication():#左下三角形
     for i in range(1,10):
         for j in range(1,i+1):
             k = i*j
-            print ("{}*{}={}".format(i,j,k),end="  ")
+            print ("{}*{}={:>2}".format(i,j,k),end="  ")
         print ("")
 
 
@@ -15,8 +15,28 @@ def leftUpMultiplication(): #左上三角形
     for i in range(1,10):
         for j in range(i,10):
             k = i*j
-            print ("{}*{}={}".format(i,j,k),end="  ")
+            print ("{}*{}={:>2}".format(i,j,k),end=" ")
         print ("")
 
 
-leftUpMultiplication()
+def RightUpMultiplication(): #右上三角形
+
+    for i in range(1,10):
+        for k in range(1,i):
+            print (end=" "*7)
+        for j in range(i,10):
+            k = i*j
+            print ("{}*{}={:>2}".format(i,j,k),end=" ")
+        print ("")
+
+
+def RightDownMultiplication():# 右下三角形
+    for i in range(1,10):
+        for k in range(1,10-i):
+            print (end=" "*7)
+        for j in range(1,i+1):
+            print ("{}*{}={:>2}".format(i,j,i*j),end=" ")
+        print (" ")
+
+
+RightDownMultiplication()
