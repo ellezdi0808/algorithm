38f8bd331960917c1fd2f8c38fbf1092d294fb78
wudi <wudi@oriental-finance.com>
2017-08-03 16:50:25 +0800

    新增两个数和的下标解决方法

--- find_sum.py
+++ find_sum.py
@@ -18,3 +18,59 @@ def sum_element(li,n):
         return "没有找到两个元素相加和为目标数"
 
 print (sum_element(listvalue,n))
+
+######################
+
+
+nums = [9,3,5,8,2,7,11,15]
+target = 20
+class Solution:
+    def twoSum(self,nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        temp = []
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    print("({},{})".format(i, j))
+                    temp.append(i)
+                    temp.append(j)
+                else:
+                    continue
+
+        return temp
+
+so = Solution()
+
+print (so.twoSum(nums,target))
+
+######################
+
+
+lists = [9,3,5,8,2,7,11,15]
+target = 20
+
+
+class Solution:
+    def twoSum(self, lists, target):
+        if len(lists) <= 1:
+            return False
+        buff_dict = {}
+        for i in range(len(lists)):
+            if lists[i] in buff_dict:
+                return [buff_dict[lists[i]], i]
+            else:
+                buff_dict[target - lists[i]] = i
+
+so = Solution()
+print (so.twoSum(lists,target))
+
+
+
+
+
+
+
